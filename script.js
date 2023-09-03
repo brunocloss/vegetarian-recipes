d2fa15cff2e95e95b8bc7c728ce909cb755dce15
Bruno Closs <14brunocloss@gmail.com>
2023-09-03 20:22:37 -0300

    3

--- script.js
+++ script.js
@@ -2,41 +2,45 @@ const recipeListEl = document.querySelector('.recipe__list');
 
 const apiKey = '95f7597b3c5f48388a71ace295b5a79f';
 const count = 10;
-const apiUrl = `https://api.spoonacular.com/recipes/random?number=${count}&apiKey=${apiKey}`;
+const apiUrl = `https://api.spoonacular.com/recipes/random?&number=${count}&apiKey=${apiKey}&diet=vegetarian`;
 
 function displayRecipes(recipes) {
     recipeListEl.innerHTML = "";
 
     recipes.forEach((recipe) => {
-        const recipeItemEl = document.createElement("li");
-        recipeItemEl.classList.add("recipe");
+        const item = document.createElement("li");
+        item.classList.add("recipe");
 
-        const recipeImageEl = document.createElement("img");
-        recipeImageEl.src = recipe.image;
-        recipeImageEl.alt = "recipe image";
+        const img = document.createElement("img");
+        img.src = recipe.image;
+        img.alt = recipe.title;
+        img.classList.add("recipe__image");
         
+        const title = document.createElement("h3");
+        title.innerText = recipe.title;
+        title.classList.add("recipe__name");
 
-        const recipeTitleEl = document.createElement("h3");
-        recipeTitleEl.innerText = recipe.title;
-
-        const recipeDescriptionEl = document.createElement("p");
-        recipeDescriptionEl.innerHTML = `
+        const description = document.createElement("p");
+        description.innerHTML = `
         <strong>Ingredients:</strong> ${recipe.extendedIngredients
           .map((ingredient) => ingredient.original)
           .join(", ")} `;
 
-        const recipeLinkEl = document.createElement("a");
-        recipeLinkEl.href = recipe.sourceUrl;
-        recipeLinkEl.innerText = "VIEW RECIPE";
+        const link = document.createElement("a");
+        link.href = recipe.sourceUrl;
+        link.innerText = "VIEW RECIPE";
+        link.classList.add("recipe__link");
 
-        recipeItemEl.appendChild(recipeImageEl);
-        recipeItemEl.appendChild(recipeTitleEl);
-        recipeItemEl.appendChild(recipeDescriptionEl);
-        recipeItemEl.appendChild(recipeLinkEl);
+        item.appendChild(img);
+        item.appendChild(title);
+        item.appendChild(description);
+        item.appendChild(link);
 
-        recipeListEl.appendChild(recipeItemEl);
+        recipeListEl.appendChild(item);
     });
 }
+
+////////////////////////////////////////////////////////
 
 async function getRecipes() {
     try {
